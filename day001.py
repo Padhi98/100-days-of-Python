22ddf0ec7f44b3268097ff803af732af0c2e3bac
Padhi98 <padhiankita1@gmail.com>
2024-07-01 23:57:24 +0530

    Add day001.py for Day 1 of 100 Days of Python

---
+++ day001.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 23:27:13 2024
+
+@author: ankitapadhi
+"""
+
+print("Welcome to the Band Name Generator.\n")
+city = input('What\'s the name of the city you grew up in?\n')
+pet = input('What is your pet\'s name?\n')
+print('Your band\'s name could be: ', city + pet)
